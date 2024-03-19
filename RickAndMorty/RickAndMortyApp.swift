3dae87bb794a87b950b42fa44ad9242394a61c08
aBugInTheSystem <ekom338.com@gmail.com>
2024-03-19 08:03:47 +0300

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Ekomobong Edeme on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
