0160178c0779531218bcfa6c001e95afe1042e01
aBugInTheSystem <ekom338.com@gmail.com>
2024-03-19 18:31:15 +0300

    Mock design added.

--- RickAndMorty/ContentView.swift
+++ RickAndMorty/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            ScrollView{
+                LazyVGrid(columns: columns) {
+                    ZStack {
+                        Color.secondary
+                        VStack(spacing:10){
+                            Image("rick")
+                                .resizable()
+                                .scaledToFit()
+                            Text("Name: Rick")
+                                .font(.title3)
+                            Text("Species:Human")
+                                .font(.title3)
+                        }
+                        .foregroundStyle(.white)
+                        .padding(.bottom)
+                    }
+                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                    .padding()
+                    
+                }
+            }
+            .navigationTitle("Rick and Morty")
         }
-        .padding()
+       
     }
 }
 
